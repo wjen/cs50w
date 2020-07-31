5d7fe0a2777fea4304cb66dfd25965ab888f6b07
Wen Jen <wjen1214@gmail.com>
2020-07-30 21:52:51 -0700

    allow search regardless of capitalization

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -11,8 +11,16 @@ def index(request):
     })
 
 def entry(request, title):
-    page = util.get_entry(title)
-    page_converted = markdowner.convert(page)
-    return render(request, 'encyclopedia/entry.html', {
-        "page" : page_converted, "title": title
-    })
+    entries = util.list_entries()
+    entries_lower = [x.lower() for x in entries]
+    if title.lower() in entries_lower:    
+        print(title)
+        page = util.get_entry(title)
+        page_converted = markdowner.convert(page)
+        return render(request, 'encyclopedia/entry.html', {
+            "page" : page_converted, "title": title
+        })
+    else:
+        return render(request, 'encyclopedia/error.html', {
+            "title": 'error'
+        })
