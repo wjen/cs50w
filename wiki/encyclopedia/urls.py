ed632e40557cc2a964949c93cc6efe524afb9b55
Wen Jen <wjen1214@gmail.com>
2020-08-02 22:14:18 -0700

    add edit url endpoint

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.show, name="show"),
     path("search", views.search, name='search'),
-    path("create", views.create, name='create')
+    path("create", views.create, name='create'),
+    path("edit/<str:title>", views.edit, name="edit")
 ]
