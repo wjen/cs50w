9c19198c0105030d3d32a3028ced17035811f922
Wen Jen <wjen1214@gmail.com>
2020-08-10 18:40:47 -0700

    add models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,21 +8,33 @@ class User(AbstractUser):
     pass
 
 
-class Comment(models.Model):
-    comment: models.CharField(max_length=300)
-    commenter: models.ForeignKey(User, on_delete=models.CASCADE)
-    listing: models.ForeignKey(Listing, on_delete=models.CASCADE)
-
-
-class Bid(models.Model):
-    price: models.IntegerField()
-    bidder: models.ForeignKey(User, on_delete=models.CASCADE)
-    listing: models.ForeignKey(Listing, on_delete=model.CASCADE)
-
-
 class Listing(models.Model):
     title: models.CharField(max_length=64)
     description: models.CharField(max_length=128, blank=True)
     image: models.URLField("Image URL Address", blank=True)
-    starting_bid: models.ForeignKey(Bid, on_delete=models.CASCADE)
-    lister: models.ForeignKey(User, on_delete=models.CASCADE)
+    price: models.DecimalField(decimal_places=2)
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
+    date_added: models.DateField("date added", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    comment: models.CharField(max_length=300)
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
+    listing: models.ForeignKey(Listing, on_delete=models.CASCADE)
+    date_added: models.DateField("date added", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.listing} -> {self.user}"
+
+
+class Bid(models.Model):
+    value: models.DecimalField(decimal_places=2)
+    user: models.ForeignKey(User, on_delete=models.CASCADE)
+    listing: models.ForeignKey(Listing, on_delete=model.CASCADE)
+    date_added: models.DateField("date added", auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.listing} -> {self.user}({self.value})"
