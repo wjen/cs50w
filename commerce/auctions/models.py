9c40193cdf6922f7d3080ba9fbfb856d82b8d8f5
Wen Jen <wjen1214@gmail.com>
2020-08-13 18:44:17 -0700

    add fields to models

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -2,11 +2,25 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
 
+CATEGORY_CHOICES = [
+    ('toys', 'Toys'),
+    ('clothes', 'Clothes'),
+    ('home', 'Home'),
+    ('electronics', 'Electronics'),
+    ('music', 'Music'),
+    ('sports', 'Sports'),
+]
 
+STATUS_CHOICES = [
+    ('active', 'Active'),
+    ('closed', 'Closed')
+]
 # Inherits from abstractuser
 
+
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField(
+        "Listing", blank=True, related_name='user_list')
 
 
 class Listing(models.Model):
@@ -16,6 +30,9 @@ class Listing(models.Model):
     value = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_added = models.DateField("date added", auto_now_add=True)
+    ended = models.BooleanField()
+    category = CATEGORY_CHOICES
+    status = STATUS_CHOICES
 
     def __str__(self):
         return self.title
@@ -27,7 +44,7 @@ class Bid(models.Model):
     listing = models.ForeignKey(
         'Listing', on_delete=models.CASCADE, related_name='bids')
     user = models.ForeignKey(
-        'User', on_delete=models.CASCADE, related_name='my_bids')
+        'User', on_delete=models.CASCADE, related_name='userBids')
 
     def __str__(self):
         return f"{self.listing} -> {self.user}({self.value})"
@@ -35,9 +52,15 @@ class Bid(models.Model):
 
 class Comment(models.Model):
     added_date = models.DateField("date added", auto_now_add=True)
-    entry = models.TextField()
+    comment = models.TextField()
     listing = models.ForeignKey('Listing', on_delete=models.CASCADE)
     user = models.ForeignKey('User', on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.listing} -> {self.user}: {self.entry}"
+
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='watching')
+    listings = models.ManyToManyField(Listing, on_delete=models.CASCADE)
